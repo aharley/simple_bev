57a15ede7da79b13560c056ac5f48d5c8f53eb48
Adam Harley <aharley@cmu.edu>
2022-08-30 16:03:35 -0700

    fixed url for rad model

--- get_rad_model.sh
+++ get_rad_model.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-THING="rad_checkpoint"
-
-echo "working on ${THING}"
-
-echo "downloading rgb model from dropbox..."
-wget https://www.dropbox.com/s/n93ryvrqyiram56/${THING}.tar.gz
+echo "downloading rad model from dropbox..."
+wget https://www.dropbox.com/s/abnzjdp0eqpfpkr/rad_checkpoint.tar.gz
 
 echo "extracting from tar..."
 tar -xvf ${THING}.tar.gz
--- get_rgb_model.sh
+++ get_rgb_model.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-THING="rgb_checkpoint"
-
-echo "working on ${THING}"
-
 echo "downloading rgb model from dropbox..."
-wget https://www.dropbox.com/s/n93ryvrqyiram56/${THING}.tar.gz
+wget https://www.dropbox.com/s/n93ryvrqyiram56/rgb_checkpoint.tar.gz
 
 echo "extracting from tar..."
 tar -xvf ${THING}.tar.gz
